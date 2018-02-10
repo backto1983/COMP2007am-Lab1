3a1fd286002fcd61318f1e7c509756b2c1f24d13
backto1983 <henrique_o@hotmail.com>
2018-02-10 15:53:23 -0500

    First commit

---
+++ Documents/Georgian College/Term 3/Enterprise Computing/Lab1/Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<string> products = new List<string>();
+            products.Add("Laptop");
+            products.Add("Tablet");
+            products.Add("Desktop");
+
+            return View();
+        }
+
+        public ActionResult Details(string product)
+        {
+            if (product == null)
+                return View("Error");
+
+            ViewBag.Product = "You selected product: " + product;     
+
+            return View();
+        }
+    }
+}
